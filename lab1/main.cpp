69e035f743adbd91444c2447ee369e1c2919bb69
Vince Fearing <vincefearing@gmail.com>
2020-02-13 10:58:24 -0800

    created class, startedimporting data

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -6,6 +6,41 @@
 using namespace std;
 
 class Data {
+    public:
+    //Getters and Setters
+    string getID() {
+        return ID;
+    }
+    void setID(string ID) {
+        this->ID = ID;
+    }
+    string getName() {
+        return name;
+    }
+    void setName(string name) {
+        this->name = name;
+    }
+    string getQuantity() {
+        return quantity;
+    }
+    void setQuantity(string quantity) {
+        this->quantity = quantity;
+    }
+    string getPrice() {
+        return price;
+    }
+    void setPrice(string price) {
+        this->price = price;
+    }
+
+    Data (string ID, string name, string quantity, string price) {
+      this-> ID = ID;
+      this-> name = name;
+      this-> quantity = quantity;
+      this-> price = price;  
+    }
+
+    private:
     string ID;
     string name;
     string quantity;
@@ -16,12 +51,27 @@ int  main() {
     vector <Data> list;
     ifstream inFile;
     string trash;
+    string ID;
+    string name;
+    string quantity;
+    string price;
+    int i = 0;
+
     inFile.open("/Volumes/Vindrive/College/Spring 2020/Intro To Data Structures/lab1/data.txt");
     if (!inFile) {
         cout << "Problem opening file";
     }
 
     while(!inFile.eof()) {
-        inFile >> list
+        inFile >> ID >> name >> quantity >> price;
+        list[i].setID(ID);
+        list[i].setName(name);
+        list[i].setPrice(price);
+        list[i].setQuantity(quantity);
+
+        ++i;
     }
+
+        cout << "is this thing working?" << endl;
+    
 }
