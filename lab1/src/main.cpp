cdd7bbfa1d4d5420b16eae8c7fcda1095dc86973
Vince Fearing <vincefearing@gmail.com>
2020-02-18 10:32:54 -0800

    worked on user menu for lab 1

--- lab1/src/main.cpp
+++ lab1/src/main.cpp
@@ -17,9 +17,11 @@ int  main()
     string name;
     string quantity;
     string price;
+    int count = 0;
     int userChoice = 0;
     bool quit = false;
 
+    //Opening file
     inFile.open("/Volumes/Vindrive/College/Spring 2020/data_structures/labs/lab1/data.txt");
     if (!inFile) 
     {
@@ -34,29 +36,64 @@ int  main()
         list.push_back(fill);
     }
 
+    //Closing file
+    inFile.close();
+
     //Letting user know if list of data is too large and which items were not included
     if (list.size() > 14)
     {
         cout << endl;
-        cout << "WARNING: File was too large only the first 15 items were imported" << endl;
+        cout << "WARNING: File was too large only 15 items were imported" << endl;
     }
-
-    inFile.close();
 
     //Menu that allows user to search for specific item or print sorted/unsorted list of all items
     while(quit != true) 
     {
         cout << endl;
         cout << "Welcome! Please choose a number from the options below." << endl << endl;
-        cout << "1. Print All" << endl;
-        cout << "2. Sort" << endl;
+        cout << "1. Print Report" << endl;
+        cout << "2. Sort Report" << endl;
         cout << "3. Search" << endl;
-        cout << "4. Exit" << endl;
+        cout << "4. Exit" << endl << endl;
         cin >> userChoice;
+
+        if(!cin)
+        {
+            cin.clear();
+            cin.ignore();
+
+            cout << "Incorrect input. Please choose a single digit corresponding your desired menu item" << endl;
+        }
 
         switch(userChoice)
         {
-            case 1 :
+            case 1 : printReport(list);
+                break;
+            case 2 : while(quit != true)
+            {
+                cout << "Sort by: " << endl;
+                cout << "1. ID" << endl;
+                cout << "2. Name" << endl;
+                cout << "3. Quantity" << endl;
+                cout << "4. Price" << endl;
+                cout << "5. Main Menu" << endl;
+                cin >> userChoice;
+
+                switch(userChoice)
+                {
+                    case 1 : //sort(userChoice);
+                        break;
+                    case 2 : //sort(userChoice);
+                        break;
+                    case 3 : //sort(userChoice);
+                        break;
+                    case 4 : //sprt(userChoice);
+                        break;
+                    case 5 : quit = true;
+                        break;
+                }
+            }
+            
         }
     }
 }
