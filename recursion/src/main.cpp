0de32411985e1da9fc41c0d8a8ed680463249f45
Vince Fearing <vincefearing@gmail.com>
2020-02-26 13:24:23 -0800

    Worked on powerOf function, needs to work with negative power

--- recursion/src/main.cpp
+++ recursion/src/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
-int main() {
-    std::cout << "Hello Easy C++ project!" << std::endl;
+using namespace std;
+
+int powerOf(int base, int power);
+
+int main() 
+{
+    int base = -5;
+    int power = -5;
+
+    cout << powerOf(base, power) << endl;
+    return 0;
+}
+
+int powerOf(int base, int power) 
+{   
+    double result = base;
+    if (power == 1) 
+    {
+        return result;
+    } else
+    {
+        return result *= powerOf(base, power-1);
+    }
+    
+    
+    
+    
 }
