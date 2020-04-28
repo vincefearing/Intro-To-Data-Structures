8d6f07e5f3159e72afd9c91fe0fb917bb19b0e13
Vince Fearing <vincefearing@gmail.com>
2020-04-27 23:59:07 -0700

    minor changes/figuring things out

--- backward-warehouse/src/StackDS.cpp
+++ backward-warehouse/src/StackDS.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 template <typename U>
-StackDS::Node<U>::Node(U dataIn)
+StackDS::Node<U>::Node(U dataIn) //goal: try and figure out how templates work for nested loops
 {
     data = dataIn;
     next = nullptr;
